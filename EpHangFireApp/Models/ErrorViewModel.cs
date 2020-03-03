f0cea2d8b169380da1390786814275131fb157a0
mondherroo <mondheriron6@gmail.com>
2020-03-02 16:51:28 -0800

    Add project files.

---
+++ EpHangFireApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EpHangFireApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
